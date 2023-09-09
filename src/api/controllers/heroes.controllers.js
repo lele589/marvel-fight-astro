b8c6ef962393051356606d340819f8b5b6e223ac
Elena <elenacfnb@gmail.com>
2023-09-09 16:48:00 +0200

    refactor(api): delete unused code

--- src/api/controllers/heroes.controllers.js
+++ src/api/controllers/heroes.controllers.js
@@ -20,32 +20,6 @@ const getHeroesList = async () => {
   }
 };
 
-// TODO: refactor
-const createHero = async (request) => {
-  try {
-    const newHero = await Hero.create(request.body);
-    return new Response(JSON.stringify(newHero), {
-      status: 201,
-      body: {
-        success: "Se ha creado el héroe",
-        user: newHero,
-      },
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: "Internal server error" }), {
-      status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    // return res.status(error.status).json({ error: error.message });
-  }
-};
-
 const getHeroById = async (params) => {
   try {
     const heroId = params.id;
@@ -80,48 +54,7 @@ const getHeroById = async (params) => {
   }
 };
 
-// TODO: refactor
-const editHeroById = async (req, res) => {
-  try {
-    const updatedHero = await Hero.update(req.body, {
-      where: { hero_id: req.query.hero_id },
-    });
-    if (!updatedHero) {
-      return res.status(404).json({ error: "El héroe no existe" });
-    }
-    return res.status(200).json({
-      success: "Se ha editado el héroe",
-      user: updatedHero,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(error.status).json({ error: error.message });
-  }
-};
-
-// TODO: refactor
-const deleteHeroById = async (req, res) => {
-  try {
-    const deletedHero = await Hero.destroy({
-      where: { hero_id: req.query.hero_id },
-    });
-    if (!deletedHero) {
-      return res.status(404).json({ error: "El héroe no existe" });
-    }
-    return res.status(200).json({
-      success: "Se ha borrado el héroe",
-      user: deletedHero,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(error.status).json({ error: error.message });
-  }
-};
-
 export const heroesController = {
   getHeroesList,
   getHeroById,
-  createHero,
-  editHeroById,
-  deleteHeroById,
 };
--- src/pages/api/heroes/index.ts
+++ src/pages/api/heroes/index.ts
@@ -4,8 +4,3 @@ import { heroesController } from '../../../api/controllers/heroes.controllers';
 export const GET:APIRoute = () =>  {
   return heroesController.getHeroesList();
 }
-
-// WIP
-export const POST:APIRoute = ({ params, request}) =>  {
-  return heroesController.createHero(request);
-}
