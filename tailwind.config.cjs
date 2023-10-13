2070241b0f193895cc2d41bff98b17bdc5351c03
Elena <elenacfnb@gmail.com>
2023-10-13 14:25:31 +0200

    chore: add flowbite dependency

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -8,7 +11,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("flowbite/plugin"), require("daisyui")],
   daisyui: {
     themes: ["light"],
   },
