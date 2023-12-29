94db3f7f06953e1be1dbffc64ba50f35ee8d9edd
Elena <elenacfnb@gmail.com>
2023-12-29 21:12:33 +0100

    chore: add Datepicker dependency

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/flowbite/**/*.js",
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js",
   ],
   theme: {
     extend: {
